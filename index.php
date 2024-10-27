335168a564f5f00b8a3351cf10f1e57350f58578
Hillel <hillel.magnichewer@etu.unilim.fr>
2024-10-27 13:47:38 +0100

    fix / iinclude

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
-<?php includ 'inc/inc.functions.php'; ?>
+<?php require_once 'inc/inc.functions.php'; ?>
 <!DOCTYPE HTML>
 <!--
 	Story by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
+<html lang="eng">
 	<head>
 		<title>Story by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<?php include 'inc/inc.css.php'; ?>
+		<?php require_once 'inc/inc.css.php'; ?>
 	</head>
 	<body class="is-preload">
 
 		<!-- Wrapper -->
 			<div id="wrapper" class="divided">
-				<?php 
+				<?php
 					getPagesTemplate(
-						array_key_exist('page', $_GET) ? $_GET['page'] : null
-					); 
+						array_key_exists('page', $_GET) ? $_GET['page'] : null
+					);
 				?>
-				<?php include 'inc/tpls-footer.php'; ?>
+				<?php require_once 'inc/tpls-footer.php'; ?>
 			</div>
 
-		<?php includes 'inc/inc.js.php'; ?>
+		<?php require_once 'inc/inc.js.php'; ?>
 
 	</body>
 </html>
